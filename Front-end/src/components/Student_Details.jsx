6f7d5304052937915bfd0a7611152c0fb6f20eb9
Prajwal <prajwalsurendra7178@gmail.com>
2023-09-17 23:49:50 +0530

    Updated Student Details

--- Front-end/src/components/Student_Details.jsx
+++ Front-end/src/components/Student_Details.jsx
@@ -1,109 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 export default function Student_Details() {
-  const styles = {
-    padding: "5px", // Reduce padding
-    border: "2px solid black",
-    margin: "5px", // Reduce margin
-  };
+  // const styles = {
+  //   padding: "5px", // Reduce padding
+  //   border: "2px solid black",
+  //   margin: "5px", // Reduce margin
+  // };
 
-  const [values, setValues] = useState({
-    Name: "",
-    USN: "",
-    semester: 1,
-    section: "A",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post("http://localhost:8081/addStudent", values)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <div>
-      <br></br>
-      <h3>Enter Student Details</h3>
-      <div className="container" style={styles}>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group mt-2">
-            <label htmlFor="studentName">Student Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="studentName"
-              name="Name"
-              placeholder="Student Name"
-              onChange={(e) => {
-                setValues({ ...values, Name: e.target.value });
-              }}
-              autoComplete="name"
-            />
-          </div>
-          <div className="form-group mt-2">
-            <label htmlFor="studentUSN">Student USN</label>
-            <input
-              type="text"
-              className="form-control"
-              id="studentUSN"
-              name="USN"
-              placeholder="Student USN"
-              onChange={(e) => {
-                setValues({ ...values, USN: e.target.value });
-              }}
-              autoComplete="usn"
-            />
-          </div>
-          <div className="form-group mt-2">
-            <label htmlFor="semester">Select Semester</label>
-            <select
-              className="form-control"
-              id="semester"
-              name="semester"
-              onChange={(e) => {
-                setValues({ ...values, semester: Number(e.target.value) });
-              }}
-              autoComplete="semester"
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </select>
-          </div>
-          <div className="form-group mt-2">
-            <label htmlFor="section">Select Section</label>
-            <select
-              className="form-control"
-              id="section"
-              name="section"
-              onChange={(e) => {
-                setValues({ ...values, section: e.target.value });
-              }}
-              autoComplete="section"
-            >
-              <option value="A">A</option>
-              <option value="B">B</option>
-              <option value="C">C</option>
-            </select>
-          </div>
-          <button type="submit" className="btn btn-primary mt-3">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
-  );
   const containerStyle = {
     padding: "20px",
     border: "2px solid black",
@@ -144,12 +47,31 @@ export default function Student_Details() {
     cursor: "pointer",
   };
 
+  const [values, setValues] = useState({
+    Name: "",
+    USN: "",
+    semester: 1,
+    section: "A",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8081/addStudent", values)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <br />
+      <br></br>
       <h3 style={headingStyle}>Enter Student Details</h3>
       <div className="container" style={containerStyle}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group mt-2">
             <label htmlFor="studentName" style={labelStyle}>
               Student Name
@@ -158,7 +80,12 @@ export default function Student_Details() {
               type="text"
               className="form-control"
               id="studentName"
+              name="Name"
               placeholder="Student Name"
+              onChange={(e) => {
+                setValues({ ...values, Name: e.target.value });
+              }}
+              autoComplete="name"
             />
           </div>
           <div className="form-group mt-2">
@@ -169,14 +96,28 @@ export default function Student_Details() {
               type="text"
               className="form-control"
               id="studentUSN"
+              name="USN"
               placeholder="Student USN"
+              onChange={(e) => {
+                setValues({ ...values, USN: e.target.value });
+              }}
+              autoComplete="usn"
             />
           </div>
           <div className="form-group mt-2">
             <label htmlFor="semester" style={labelStyle}>
               Select Semester
             </label>
-            <select className="form-control" id="semester" style={selectStyle}>
+            <select
+              className="form-control"
+              id="semester"
+              name="semester"
+              onChange={(e) => {
+                setValues({ ...values, semester: Number(e.target.value) });
+              }}
+              autoComplete="semester"
+              style={selectStyle}
+            >
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -191,7 +132,16 @@ export default function Student_Details() {
             <label htmlFor="section" style={labelStyle}>
               Select Section
             </label>
-            <select className="form-control" id="section" style={selectStyle}>
+            <select
+              className="form-control"
+              id="section"
+              name="section"
+              onChange={(e) => {
+                setValues({ ...values, section: e.target.value });
+              }}
+              autoComplete="section"
+              style={selectStyle}
+            >
               <option value="A">A</option>
               <option value="B">B</option>
               <option value="C">C</option>
